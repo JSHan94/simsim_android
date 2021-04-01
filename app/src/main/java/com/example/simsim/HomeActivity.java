86366b185b014fc05c8d2ca4b51343b4916206f1
한정수(컴퓨터공학과) <saw1515@postech.ac.kr>
2021-04-01 18:41:31 +0900

    Develop match view list in HomeActivity

--- app/src/main/java/com/example/simsim/HomeActivity.java
+++ app/src/main/java/com/example/simsim/HomeActivity.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.example.simsim.adapter.MatchAdapter;
+import com.example.simsim.dataclass.MatchData;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,6 +24,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -25,6 +35,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button tagBtn;
     private EditText tagEditText;
+
+    ArrayList<MatchData> matchDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,25 +50,50 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         //firebase
         user = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference("Users");
+        if(user != null){
+            userID = user.getUid();
+            reference = FirebaseDatabase.getInstance().getReference("Users");
+            this.getMatchData();
+        }
 
 
         tagBtn.setOnClickListener(this);
 
+        ListView matchListView = (ListView) findViewById(R.id.matchListView);
+        final MatchAdapter matchAdapter = new MatchAdapter(this,matchDataList);
+        matchListView.setAdapter(matchAdapter);
 
-//        //한번 유저 정보 호출
-//        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                //User
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
+        matchListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                Toast.makeText(HomeActivity.this,
+                        "Clicked!!",
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
+    }
+
+    private void getMatchData() {
+        matchDataList = new ArrayList<MatchData>();
+        //get Matchdata , can change to addListenerForSingleValueEvent
+        reference.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+
+                }
+            }
+        });
+        matchDataList.add(new MatchData("정수","Game",100));
+        matchDataList.add(new MatchData("재준","Game",100));
     }
 
     @Override
--- app/src/main/java/com/example/simsim/adapter/MatchAdapter.java
+++ app/src/main/java/com/example/simsim/adapter/MatchAdapter.java
@@ -4,8 +4,15 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.simsim.HomeActivity;
+import com.example.simsim.R;
 import com.example.simsim.dataclass.MatchData;
 
 import java.util.ArrayList;
@@ -15,6 +22,11 @@ public class MatchAdapter extends BaseAdapter {
     Context mContext = null;
     LayoutInflater mLayoutInflater = null;
     ArrayList<MatchData> data;
+    private Button matchBtn;
+
+    public MatchAdapter(){
+
+    }
 
     public MatchAdapter(Context context, ArrayList<MatchData> data){
         mContext = context;
@@ -39,11 +51,22 @@ public class MatchAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        View view = mLayoutInflater.inflate(R.layout.row,null);
+
+
+        TextView userNameView = (TextView) view.findViewById(R.id.userName);
+        TextView userTagView = (TextView) view.findViewById(R.id.userTag);
+        Button matchBtn = (Button) view.findViewById(R.id.matchBtn);
+
+
 
         String tag = data.get(position).getTag();
         String userName = data.get(position).getUserName();
         int distance = data.get(position).getDistance();
 
-        return null;
+        userNameView.setText(userName);
+        userTagView.setText(tag);
+
+        return view;
     }
 }
